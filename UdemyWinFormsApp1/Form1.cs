260c63cae4df9923216fa8a27e8495207128d58f
Mike Walker <mike.walker3939@gmail.com>
2019-02-27 11:26:33 -0600

    add initial code

---
+++ UdemyWinFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UdemyWinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            SayHello();
+        }
+
+        private void SayHello()
+        {
+            MessageBox.Show("Hello");
+        }
+        #endregion
+
+    }
+}
