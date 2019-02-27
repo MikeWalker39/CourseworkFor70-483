b59442adf80c149ec0acdb7fbb299eaab42c21b7
Mike Walker <mike.walker3939@gmail.com>
2019-02-27 12:10:16 -0600

    add gauge, trackbar, methods to handle, try catches

--- UdemyWinFormsApp1/Form1.cs
+++ UdemyWinFormsApp1/Form1.cs
@@ -14,26 +14,99 @@ namespace UdemyWinFormsApp1
     {
         public Form1()
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
-
-        #region 
-
+                
         private void Form1_Load(object sender, EventArgs e)
         {
+            try
+            {
+                InitialSetup();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
 
         }
 
         private void simpleButton1_Click(object sender, EventArgs e)
         {
-            SayHello();
+            try
+            {
+                SayHello();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         private void SayHello()
         {
-            MessageBox.Show("Hello");
+            try
+            {
+                MessageBox.Show("Hello");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        #region gaugeTest
+        private void InitialSetup()
+        {
+            try
+            {
+                arcScaleComponent1.Value = 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (arcScaleComponent1.Value == 0) arcScaleComponent1.Value = 1;
+
+                arcScaleComponent1.Value = (int)(arcScaleComponent1.Value * 2);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        private void trackBarControl1_EditValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                arcScaleComponent1.Value = trackBarControl1.Value * 10;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
         #endregion
-
     }
 }
